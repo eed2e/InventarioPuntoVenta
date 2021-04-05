e312f059531cfc07cf826370591b5aca62565e81
eed2e <eed.esteban@gmail.com>
2021-04-05 13:07:13 -0500

    actualiza registro

--- sistema/registro_producto.php
+++ sistema/registro_producto.php
@@ -2,18 +2,16 @@
   include "../conexion.php";
   if (!empty($_POST)) {
     $alert = "";
-    if (empty($_POST['proveedor']) || empty($_POST['producto']) || empty($_POST['precio']) || $_POST['precio'] <  0 || empty($_POST['cantidad'] || $_POST['cantidad'] <  0)) {
+    if (empty($_POST['producto']) || empty($_POST['cantidad'] || $_POST['cantidad'] <  0)) {
       $alert = '<div class="alert alert-danger" role="alert">
                 Todo los campos son obligatorios
               </div>';
     } else {
-      $proveedor = $_POST['proveedor'];
       $producto = $_POST['producto'];
-      $precio = $_POST['precio'];
       $cantidad = $_POST['cantidad'];
       $usuario_id = $_SESSION['idUser'];
 
-      $query_insert = mysqli_query($conexion, "INSERT INTO producto(proveedor,descripcion,precio,existencia,usuario_id) values ('$proveedor', '$producto', '$precio', '$cantidad','$usuario_id')");
+      $query_insert = mysqli_query($conexion, "INSERT INTO producto(descripcion,existencia,usuario_id) values ( '$producto','$cantidad','$usuario_id')");
       if ($query_insert) {
         $alert = '<div class="alert alert-primary" role="alert">
                 Producto Registrado
@@ -42,33 +40,12 @@
        <form action="" method="post" autocomplete="off">
          <?php echo isset($alert) ? $alert : ''; ?>
          <div class="form-group">
-           <label>Proveedor</label>
-           <?php
-            $query_proveedor = mysqli_query($conexion, "SELECT codproveedor, proveedor FROM proveedor ORDER BY proveedor ASC");
-            $resultado_proveedor = mysqli_num_rows($query_proveedor);
-            mysqli_close($conexion);
-            ?>
-           <select id="proveedor" name="proveedor" class="form-control">
-             <?php
-              if ($resultado_proveedor > 0) {
-                while ($proveedor = mysqli_fetch_array($query_proveedor)) {
-                  // code...
-              ?>
-                 <option value="<?php echo $proveedor['codproveedor']; ?>"><?php echo $proveedor['proveedor']; ?></option>
-             <?php
-                }
-              }
-              ?>
-           </select>
-         </div>
+            
          <div class="form-group">
            <label for="producto">Producto</label>
            <input type="text" placeholder="Ingrese nombre del producto" name="producto" id="producto" class="form-control">
          </div>
-         <div class="form-group">
-           <label for="precio">Precio</label>
-           <input type="text" placeholder="Ingrese precio" class="form-control" name="precio" id="precio">
-         </div>
+         
          <div class="form-group">
            <label for="cantidad">Cantidad</label>
            <input type="number" placeholder="Ingrese cantidad" class="form-control" name="cantidad" id="cantidad">
@@ -80,4 +57,3 @@
 
 
  </div>
- <?php include_once "includes/footer.php"; ?>
